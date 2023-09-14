9ebbac6147a39a594e26222886867cbb49a48edc
Kevin dos Reis Hartwig <kevindosreishartwig1221@gmail.com>
2023-09-14 19:35:40 -0300

    first class: Positon

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Jogo de xadrez, play go!!!");
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
